0f5a901827a4f61b6b077968da6c3131dfc901b8
Samantha Soto <samantha.sotoalejos@gmail.com>
2019-07-25 09:23:58 -0700

    User selects a date

--- main.py
+++ main.py
@@ -21,14 +21,17 @@ class InputPage(webapp2.RequestHandler):
         start_template = jinja_current_dir.get_template("templates/InputPage.html")
         self.response.write(start_template.render())
 
-        # start_string = self.request.get('starttime')
-        # start_date = datetime.strptime(start_string, "%m/%d/%Y")
-        #
+    def post(self):
+        start_string = self.request.get('starttime')
+        start_date = datetime.strptime(start_string, "%Y-%m-%d")
+
         # calendar_url = "http://www.google.com/calendar/event?action=TEMPLATE&text=%s&dates=%s/%s"
-        #
-        # calendar_link = calendar_url % ("TestEvent", calendar_start, calendar_end)
+
+        # calendar_link = calendar_url % ("TestEvent", 7, 12) #calendar_start, calendar_end)
         # calendar_html = "<HTML><BODY><A href='%s' target='_blank'>Test Event Link</A></BODY></HTML>"
         # self.response.write(calendar_html % calendar_link)
+
+# reminder vs calendar; didnt upload to github
 
 
 
