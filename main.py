827f314f0422838228324e762f6c5e84dffc7beb
Alexandra Lai <alexandralai00@gmail.com>
2019-07-24 17:21:25 -0700

    Added classes

--- main.py
+++ main.py
@@ -10,42 +10,50 @@ jinja_current_dir = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class FoodHandler(webapp2.RequestHandler):
+class Mainpage(webapp2.RequestHandler):
     def get(self):
-        start_template = jinja_current_dir.get_template("templates/welcome.html")
+        start_template = jinja_current_dir.get_template("templates/MainPage.html")
         self.response.write(start_template.render())
 
-    def post(self):
-        user = users.get_current_user()
-        the_fav_food = self.request.get('user-fav-food')
-
-        #put into database (optional)
-        food_record = Food(food_name = the_fav_food)
-        food_record.user_id = user.user_id()
-        food_record.put()
-
-        #pass to the template via a dictionary
-        variable_dict = {'fav_food_for_view': the_fav_food}
-        end_template = jinja_current_dir.get_template("templates/results.html")
-        self.response.write(end_template.render(variable_dict))
-
-class ShowFoodHandler(webapp2.RequestHandler):
+class InputPage(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        food_list_template = jinja_current_dir.get_template("templates/foodlist.html")
-        your_foods = Food.query().filter(Food.user_id == user.user_id()).order(-Food.food_name).fetch(3)
-        fav_foods = Food.query().order(-Food.food_name).fetch(3)
-        dict_for_template = {
-            'top_fav_foods': fav_foods,
-            'your_fav_foods': your_foods,
-        }
-        self.response.write(food_list_template.render(dict_for_template))
-
-app = webapp2.WSGIApplication([
-    ('/', FoodHandler),
-    ('/showfavs', ShowFoodHandler)
-], debug=True)
+        start_template = jinja_current_dir.get_template("templates/InputPage.html")
+        self.response.write(start_template.render())
+#
+#     def post(self):
+#         user = users.get_current_user()
+#         the_fav_food = self.request.get('user-fav-food')
+#
+#         #put into database (optional)
+#         food_record = Food(food_name = the_fav_food)
+#         food_record.user_id = user.user_id()
+#         food_record.put()
+#
+#         #pass to the template via a dictionary
+#         variable_dict = {'fav_food_for_view': the_fav_food}
+#         end_template = jinja_current_dir.get_template("templates/results.html")
+#         self.response.write(end_template.render(variable_dict))
+#
+# class ShowFoodHandler(webapp2.RequestHandler):
+#     def get(self):
+#         user = users.get_current_user()
+#         food_list_template = jinja_current_dir.get_template("templates/foodlist.html")
+#         your_foods = Food.query().filter(Food.user_id == user.user_id()).order(-Food.food_name).fetch(3)
+#         fav_foods = Food.query().order(-Food.food_name).fetch(3)
+#         dict_for_template = {
+#             'top_fav_foods': fav_foods,
+#             'your_fav_foods': your_foods,
+#         }
+#         self.response.write(food_list_template.render(dict_for_template))
 #
 # app = webapp2.WSGIApplication([
-#     ('/', MainPage),
+#     ('/', FoodHandler),
+#     ('/showfavs', ShowFoodHandler)
 # ], debug=True)
+# #
+app = webapp2.WSGIApplication([
+    ('/', Mainpage),
+    ('/input', InputPage),
+    # ('/inventory', InventoryPage),
+    # ('/recipes', RecipePage)
+], debug=True)
